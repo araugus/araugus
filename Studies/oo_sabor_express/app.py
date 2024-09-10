266c4a3b334b0446efde20e0607be9a555805343
Gustavo Araujo <galgustavo@outlook.comgit config user.email galgustavo@outlook.com>
2024-09-10 18:03:11 -0300

    OO Study Finalized

--- Studies/oo_sabor_express/app.py
+++ Studies/oo_sabor_express/app.py
@@ -1,12 +1,24 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
+from modelos.cardapio.sobremesa import Sobremesa
 
 restaurante_japa = Restaurante('DJapa', 'Japonesa')
-restaurante_japa.receber_avaliacao('Gus', 4)
-restaurante_japa.receber_avaliacao('Luis', 5)
-restaurante_japa.receber_avaliacao('Amanda', 2)
+
+coca_cola = Bebida('Coca Cola', 5, '500ml')
+coca_cola.aplicar_desconto()
+
+sushi = Prato('Sushi', 35, 'Sushi de Salmão')
+sushi.aplicar_desconto()
+
+brownie = Sobremesa('Brownie',15,'Fatia')
+
+restaurante_japa.adicionar_no_cardapio(coca_cola)
+restaurante_japa.adicionar_no_cardapio(sushi)
+restaurante_japa.adicionar_no_cardapio(brownie)
 
 def main():
-    Restaurante.listar_restaurantes()
+    restaurante_japa.exibir_cardapio
 
 if __name__ == '__main__':
     main()
